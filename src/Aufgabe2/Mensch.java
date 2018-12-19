439a1d50ea5a651ec9621b59452f10ef54b66d6b
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-12-19 08:14:54 +0100

    Aufgabe 2

--- src/Aufgabe2/Mensch.java
+++ src/Aufgabe2/Mensch.java
@@ -1,4 +1,49 @@
 package Aufgabe2;
 
 public class Mensch {
+
+    private String vorname;
+    private String nachname;
+    private int alter;
+
+    public Mensch(String vorname, String nachname, int alter)
+    {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.alter = alter;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public int getAlter() {
+        return alter;
+    }
+
+    public void setAlter(int alter) {
+        this.alter = alter;
+    }
+
+
+
+
+
+    public void Status()
+    {
+        System.out.println(String.format("Es ist der Mensch "+ getVorname()+ " "+ getNachname()+", "+ getAlter()));
+    }
+
 }
